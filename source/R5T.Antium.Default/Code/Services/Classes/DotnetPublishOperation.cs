43bce7a5039b91bb877e7b6bab08ff04eccf794b
MinexAutomation <minex.automation@gmail.com>
2019-10-29 22:41:43 -0700

    Added dotnet publish operation.

---
+++ source/R5T.Antium.Default/Code/Services/Classes/DotnetPublishOperation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using R5T.Norsica;
+using R5T.Pompeii;
+
+
+namespace R5T.Antium.Default
+{
+    public class DotnetPublishOperation : IPublishOperation
+    {
+        private IEntryPointProjectFilePathProvider EntryPointProjectFilePathProvider { get; }
+        private IEntryPointProjectBuildOutputPublishDirectoryPathProvider EntryPointProjectBuildOutputPublishDirectoryPathProvider { get; }
+        private IDotnetCommandLineOperator DotnetCommandLineOperator { get; }
+
+
+        public DotnetPublishOperation(
+            IEntryPointProjectFilePathProvider entryPointProjectFilePathProvider,
+            IDotnetCommandLineOperator dotnetCommandLineOperator,
+            IEntryPointProjectBuildOutputPublishDirectoryPathProvider entryPointProjectBuildOutputPublishDirectoryPathProvider)
+        {
+            this.EntryPointProjectFilePathProvider = entryPointProjectFilePathProvider;
+            this.DotnetCommandLineOperator = dotnetCommandLineOperator;
+            this.EntryPointProjectBuildOutputPublishDirectoryPathProvider = entryPointProjectBuildOutputPublishDirectoryPathProvider;
+        }
+
+        public void Execute()
+        {
+            var projectFilePath = this.EntryPointProjectFilePathProvider.GetEntryPointProjectFilePath();
+
+            var publishDirectoryPath = this.EntryPointProjectBuildOutputPublishDirectoryPathProvider.GetEntryPointProjectBuildOutputPublishDirectoryPath();
+
+            this.DotnetCommandLineOperator.PublishApp(projectFilePath, publishDirectoryPath);
+        }
+    }
+}

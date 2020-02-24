d392ea894b1e0cd7617e7aeb371c2f159dd64674
MinexAutomation <minex.automation@gmail.com>
2020-02-23 16:49:51 -0800

    Implemented deployment source and remote destination secrets directory file system site providers.

---
+++ source/R5T.Antium.Default/Code/Services/Classes/DefaultDeploymentSource_SecretsDirectory_FileSystemSiteProvider.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using R5T.Gepidia;
+using R5T.Gepidia.Local;
+using R5T.Suebia;
+
+
+namespace R5T.Antium.Default
+{
+    public class DefaultDeploymentSource_SecretsDirectory_FileSystemSiteProvider : IDeploymentSource_SecretsDirectory_FileSystemSiteProvider
+    {
+        private ILocalFileSystemOperator LocalFileSystemOperator { get; }
+        private ISecretsDirectoryPathProvider SecretsDirectoryPathProvider { get; }
+
+        
+        public DefaultDeploymentSource_SecretsDirectory_FileSystemSiteProvider(
+            ILocalFileSystemOperator localFileSystemOperator,
+            ISecretsDirectoryPathProvider secretsDirectoryPathProvider)
+        {
+            this.LocalFileSystemOperator = localFileSystemOperator;
+            this.SecretsDirectoryPathProvider = secretsDirectoryPathProvider;
+        }
+
+        public FileSystemSite GetDeploymentSourceSecretsDirectoryFileSystemSite()
+        {
+            var secretsDirectoryPath = this.SecretsDirectoryPathProvider.GetSecretsDirectoryPath();
+
+            var secretsDirectoryFileSystemSite = new FileSystemSite(secretsDirectoryPath, this.LocalFileSystemOperator);
+            return secretsDirectoryFileSystemSite;
+        }
+    }
+}
---
+++ source/R5T.Antium.Default/Code/Services/Classes/DefaultRemoteDeploymentDestination_SecretsDirectory_FileSystemSiteProvider.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using R5T.Gepidia;
+using R5T.Gepidia.Remote;
+using R5T.Lombardy;
+
+
+namespace R5T.Antium.Default
+{
+    public class DefaultRemoteDeploymentDestination_SecretsDirectory_FileSystemSiteProvider : IDeploymentDestination_SecretsDirectory_FileSystemSiteProvider
+    {
+        private IRemoteFileSystemOperator RemoteFileSystemOperator { get; }
+        private IRemoteDeploymentSecretsSerializationProvider RemoteDeploymentSecretsSerializationProvider { get; }
+
+
+        public DefaultRemoteDeploymentDestination_SecretsDirectory_FileSystemSiteProvider(
+            IRemoteFileSystemOperator remoteFileSystemOperator,
+            IRemoteDeploymentSecretsSerializationProvider remoteDeploymentSecretsSerializationProvider)
+        {
+            this.RemoteFileSystemOperator = remoteFileSystemOperator;
+            this.RemoteDeploymentSecretsSerializationProvider = remoteDeploymentSecretsSerializationProvider;
+        }
+
+        public FileSystemSite GetDeploymentDestinationSecretsDirectoryFileSystemSite()
+        {
+            var remoteDeploymentSecretsSerialization = this.RemoteDeploymentSecretsSerializationProvider.GetRemoteDeploymentSecretsSerialization();
+
+            var remoteSecretsDirectoryPath = remoteDeploymentSecretsSerialization.DirectoryPath; // Put secrets files into the same directory as all the code.
+
+            var remoteSecretsDirectoryFileSystemSite = new FileSystemSite(remoteSecretsDirectoryPath, this.RemoteFileSystemOperator);
+            return remoteSecretsDirectoryFileSystemSite;
+        }
+    }
+}

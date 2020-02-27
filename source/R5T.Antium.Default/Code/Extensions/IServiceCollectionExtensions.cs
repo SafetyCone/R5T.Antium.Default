4426e63284c8da10cbc3c6b1b1d12aacfb8ed408
MinexAutomation <minex.automation@gmail.com>
2020-02-27 00:18:09 -0800

    Added IServiceCollection extensions.

--- source/R5T.Antium.Default/Code/Extensions/IServiceCollectionExtensions.cs
+++ source/R5T.Antium.Default/Code/Extensions/IServiceCollectionExtensions.cs
@@ -293,5 +293,63 @@ namespace R5T.Antium.Default
                 addDotnetOperator));
             return serviceAction;
         }
+
+        /// <summary>
+        /// Adds the <see cref="DefaultRemoteDeploymentDestination_SecretsDirectory_FileSystemSiteProvider"/> implementation of <see cref="IDeploymentDestination_SecretsDirectory_FileSystemSiteProvider"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceCollection AddDefaultRemoteDeploymentDestination_SecretsDirectory_FileSystemSiteProvider(this IServiceCollection services,
+            ServiceAction<IRemoteFileSystemOperator> addRemoteFileSystemOperator,
+            ServiceAction<IRemoteDeploymentSecretsSerializationProvider> addRemoteDeploymentSecretsSerializationProvider)
+        {
+            services
+                .AddSingleton<IDeploymentDestination_SecretsDirectory_FileSystemSiteProvider, DefaultRemoteDeploymentDestination_SecretsDirectory_FileSystemSiteProvider>()
+                .RunServiceAction(addRemoteFileSystemOperator)
+                .RunServiceAction(addRemoteDeploymentSecretsSerializationProvider)
+                ;
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="DefaultRemoteDeploymentDestination_SecretsDirectory_FileSystemSiteProvider"/> implementation of <see cref="IDeploymentDestination_SecretsDirectory_FileSystemSiteProvider"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static ServiceAction<IDeploymentDestination_SecretsDirectory_FileSystemSiteProvider> AddDefaultRemoteDeploymentDestination_SecretsDirectory_FileSystemSiteProviderAction(this IServiceCollection services,
+            ServiceAction<IRemoteFileSystemOperator> addRemoteFileSystemOperator,
+            ServiceAction<IRemoteDeploymentSecretsSerializationProvider> addRemoteDeploymentSecretsSerializationProvider)
+        {
+            var serviceAction = new ServiceAction<IDeploymentDestination_SecretsDirectory_FileSystemSiteProvider>(() => services.AddDefaultRemoteDeploymentDestination_SecretsDirectory_FileSystemSiteProvider(
+                addRemoteFileSystemOperator,
+                addRemoteDeploymentSecretsSerializationProvider));
+            return serviceAction;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="DefaultDeploymentSource_SecretsDirectory_FileSystemSiteProvider"/> implementation of <see cref="IDeploymentSource_SecretsDirectory_FileSystemSiteProvider"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceCollection AddDefaultDeploymentSource_SecretsDirectory_FileSystemSiteProvider(this IServiceCollection services,
+            ServiceAction<ILocalFileSystemOperator> addLocalFileSystemOperator,
+            ServiceAction<ISecretsDirectoryPathProvider> addSecretsDirectoryPathProvider)
+        {
+            services
+                .AddSingleton<IDeploymentSource_SecretsDirectory_FileSystemSiteProvider, DefaultDeploymentSource_SecretsDirectory_FileSystemSiteProvider>()
+                .RunServiceAction(addLocalFileSystemOperator)
+                .RunServiceAction(addSecretsDirectoryPathProvider)
+                ;
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="DefaultDeploymentSource_SecretsDirectory_FileSystemSiteProvider"/> implementation of <see cref="IDeploymentSource_SecretsDirectory_FileSystemSiteProvider"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static ServiceAction<IDeploymentSource_SecretsDirectory_FileSystemSiteProvider> AddDefaultDeploymentSource_SecretsDirectory_FileSystemSiteProviderAction(this IServiceCollection services,
+            ServiceAction<ILocalFileSystemOperator> addLocalFileSystemOperator,
+            ServiceAction<ISecretsDirectoryPathProvider> addSecretsDirectoryPathProvider)
+        {
+            var serviceAction = new ServiceAction<IDeploymentSource_SecretsDirectory_FileSystemSiteProvider>(() => services.AddDefaultDeploymentSource_SecretsDirectory_FileSystemSiteProvider(
+                addLocalFileSystemOperator,
+                addSecretsDirectoryPathProvider));
+            return serviceAction;
+        }
     }
 }
